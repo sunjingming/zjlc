8bb6f6cff2eb35f35700f765d9807ca5b40d6557
17180134925 <17180134925@163.com>
2016-12-02 14:19:03 +0800

    sss

--- app/src/main/java/com/example/administrator/zjlc/pager/Account.java
+++ app/src/main/java/com/example/administrator/zjlc/pager/Account.java
@@ -11,17 +11,17 @@ import com.example.administrator.zjlc.base.BasePager;
 /**
  * Created by Administrator on 2016/6/23.
  */
-public class LiCaiPager extends BasePager {
+public class Account extends BasePager {
 
     private View view;
 
-    public LiCaiPager(Activity activity) {
+    public Account(Activity activity) {
         super(activity);
     }
 
     @Override
-    public void initData() {
-        super.initData();
+    public void initData() {super.initData();
+
         System.out.println("理财数据被初始化了...");
         //设置标题
         view = View.inflate(mActivity, R.layout.licaipager, null);
